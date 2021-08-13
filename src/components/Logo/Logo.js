650eef1791eefb29ba4d5f0853588f3dc49565cf
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-14 02:24:39 +0430

    logo code update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Logo.css';
 import Tilt from 'react-tilt';
 import robot from './robot.svg';
+import './Logo.css';
 
 const Logo = () => {
 	return(
-		<div className="center">
+		<div className="robot-container">
 			<Tilt className="Tilt ma4 mt0" options={{ max : 10 }} style={{ height: 150, width: 150 }} >
 				<div className="Tilt-inner"> <img src={robot} alt='robot'/> </div>
 			</Tilt>
