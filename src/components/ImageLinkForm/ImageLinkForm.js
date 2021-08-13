fec12cfafa751ca5502ee6a585b65a5bd20bcccd
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-14 02:22:30 +0430

    code refactor + nav update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
 	return(
 		<div className='tc'>
 			<p className='b'>{'Anton will detect faces in your pictures. Give it a try!'}</p>
-			<input className='f4 pa1 w-40' type='text'/>
-			<button className='w-10 grow f4 link ph2 pv1 dib white bg-light-purple'>Detect</button>
+			<div className="center">
+				<div className='bg box'>
+					<input className='f4 pa1' type='text'/>
+					<button className='grow f4 link ph2 pv1 dib white bg-light-purple'>Detect</button>
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ import './Navigation.css';
 const Navigation = () => {
 	return(
 		<nav className='nav'>
-			<p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+			<p className='sign-out'>Sign Out</p>
 		</nav>
 	);
 };
