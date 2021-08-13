bbdc1a8ed943578565de4822e064a4592294fcf0
Hamed Feizabadi <hamedfz19@gmail.com>
2021-08-14 04:08:51 +0430

    image link form component update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,13 +3,11 @@ import './ImageLinkForm.css'
 
 const ImageLinkForm = () => {
 	return(
-		<div className='tc'>
-			<p className='b'>{'Anton will detect faces in your pictures. Give it a try!'}</p>
-			<div className="center">
-				<div className='bg box'>
-					<input className='f4 pa1' type='text'/>
-					<button className='grow f4 link ph2 pv1 dib white bg-light-purple'>Detect</button>
-				</div>
+		<div className='ilf-outer'>
+			<p className='ilf-p'>{'Anton will detect faces in your pictures. Give it a try!'}</p>
+			<div className='ilf-inner ilf-bg'>
+					<input className='ilf-input' type='text'/>
+					<button className="ilf-btn glow-on-hover"><span>Detect </span></button>
 			</div>
 		</div>
 	);
